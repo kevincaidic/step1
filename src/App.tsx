a3ddacfb5575334ea002bf2dffe76e0443c11cb2
Kevin <kevin.caidic1000@gmail.com>
2026-06-04 10:46:09 +0800

    rollback

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export default function App() {
   };
 
   return (
-    <div className="min-h-screen bg-[#008195] text-white font-sans antialiased text-sm flex flex-col justify-between">
+    <div className="min-h-screen bg-white text-slate-900 font-sans antialiased text-sm flex flex-col justify-between">
       {/* Dynamic Navigation glass elements */}
       <Navigation />
 
